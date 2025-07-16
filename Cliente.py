5c5bc2d4235206a4cc7759493168244956a8dd32
Edwin Chusin <edwinchusin2005@gmail.com>
2025-07-15 21:16:17 -0500

    Añadi codigo para registro, inicar sesion, cargar datos y un menu basico... tiene el problema de duplicar los usuarios y claves

--- Cliente.py
+++ Cliente.py
@@ -1,16 +1,87 @@
 
 #Apartado del cliente
+usuarios = []
 from io import open
 
-def crear_archivo(archivo):
+def crear_archivo(archivo, usuarios):
     try:    
-        with open(archivo, "w", encoding="UTF-8")as f:
-            f.write("Archivo.txt")
+        with open(archivo, "a", encoding="UTF-8")as f:
+            for i in usuarios:
+                f.write(f"{i['Usuario']},  {i['Clave']}\n")
             print("Archivo creado correctamente. ")
     except Exception as e:
         print(f"Error al crear el archivo {e}")
 
-def registrar_usuario():
+
+def registrar_usuario(usuarios):
+    print("\n REGISTRO ")
+    usuario = input("Ingrese el usuario: ")
+    contraseña = input("Ingrese su contraseña: ")
+    usuarios.append({"Usuario" : usuario, "Clave" : contraseña})
+    print("Usted se ha registrado correctamente. ")
+
+def login_usuario(usuarios):
+    if not usuarios:
+        print("No existen usuarios registrados. ")
+        return
+    usuario = input("Usuario: ")
+    contraseña = input("Contraseña: ")
+
+    for i in usuarios:
+        if i ["Usuario"] == usuario and i["Clave"] == contraseña:
+            print("Bienvenido al sistema. ")
+            return
+    print("Usuario o contraseña incorrectos...\n")
+
+def cargar_datos(archivo):
+    try:
+        with open(archivo, "r", encoding = "utf-8") as archivo_texto:
+            for linea in archivo_texto:
+                linea = linea.strip() #Elimina saltos de linea
+                if linea: #La linea no esta vacia
+                    usuario, contraseña = linea.split(",")
+                    usuarios.append({"Usuario": usuario.strip(), "Clave": contraseña.strip()})
+        return usuarios
+    except FileNotFoundError:
+        print("El archivo no existe")
+        return[]
+
+def menu():
+    print("\n")
+    print("/"*40)
+    print("Sistema de TURISMO")
+    print("/"*40)
+    print("1. Registrar usuario")
+    print("2. Iniciar sesion")
+    print("3. Salir")
+
     
+def main():
+    archivo = "usuarios.txt"
+    lista_usuarios = cargar_datos(archivo)
+    
+    while True:
+        menu()
+        try:
+            opcion = int(input("Ingrese una de las opciones: "))
+            if opcion == 1:
+                registrar_usuario(lista_usuarios)
+                crear_archivo(archivo, lista_usuarios)
+            elif opcion == 2:
+                login_usuario(lista_usuarios)
+            elif opcion == 3:
+                print("saliendo del sistema. ")
+                break
+            else:
+                print("Opcion no valida...")
+        except ValueError:
+            print("Digite solo numeros.")
+
+main()
+
+
+
+
+
 
 
