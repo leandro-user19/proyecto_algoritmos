4e449cf5a23fecac28956e9726747a91f0a2c004
maykolaico <maykolaico@gmail.com>
2025-07-22 21:38:24 -0500

    Add listar_puntos_turisticos function to display tourist points

---
+++ listar_puntos.py
@@ -0,0 +1,7 @@
+def listar_puntos_turisticos(puntos_turisticos):
+    if not puntos_turisticos:
+        print("No hay puntos turísticos registrados.") 
+        return
+    print("Lista de puntos turísticos:")
+    for punto in puntos_turisticos:
+        pass
